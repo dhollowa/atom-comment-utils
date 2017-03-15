1a39dc59fc5cacc47829072048be6f0d362c73ff
David Holloway <dhollowa@google.com>
2017-03-15 13:24:10 -0700

    First commit. Just a hello-world scaffold.

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 0.0.0 - First Release
+* comment-cycle supported on multiple selections.
---
+++ spec/comment_utils-spec.js
@@ -0,0 +1,11 @@
+'use babel';
+
+import pipeUtils from '../lib/comment_utils';
+
+describe('CommentUtils', () => {
+  describe('when something happens', () => {
+    it('compares apples to apples', () => {
+      expect("apples").toEqual("apples")
+    })
+  })
+})
